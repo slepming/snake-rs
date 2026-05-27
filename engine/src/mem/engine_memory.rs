b732abce8b7872a2bfb99a78d7751a6a0c520ee8
slepming <siskaura06@gmail.com>
2026-05-27 21:44:05 +0300

    change memory block size for memory allocator

--- engine/src/mem/engine_memory.rs
+++ engine/src/mem/engine_memory.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use vulkano::{
     command_buffer::allocator::StandardCommandBufferAllocator,
     descriptor_set::allocator::StandardDescriptorSetAllocator, device::Device,
-    memory::allocator::StandardMemoryAllocator,
+    memory::allocator::{GenericMemoryAllocatorCreateInfo, StandardMemoryAllocator},
 };
 
 /// Engine memory allocators storage
@@ -15,7 +15,10 @@ pub(crate) struct EngineMemory {
 
 impl EngineMemory {
     pub fn new(device: Arc<Device>) -> Self {
-        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+        // Block size 64 MB
+        let memory_type_count = device.physical_device().memory_properties().memory_types.len();
+        let block_size = vec![1024 * 1024 * 64; memory_type_count];
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new(device.clone(), GenericMemoryAllocatorCreateInfo { block_sizes: &block_size,..Default::default() }));
 
         // Before we can start creating and recording command buffers, we need a way of allocating
         // them. Vulkano provides a command buffer allocator, which manages raw Vulkan command
