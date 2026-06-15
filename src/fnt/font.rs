ea0fecbc66b65aab6029fe2452324f7e69f7fd5d
slepming <siskaura06@gmail.com>
2026-06-15 22:38:52 +0300

    add font module

--- src/fnt/font.rs
+++ src/fnt/font.rs
@@ -1,5 +1,43 @@
-use crate::drw::drawable::Drawable;
+use std::path::Path;
 
-pub struct SpriteText {
-    drawable: Drawable
+use crate::res::assets::Storage;
+
+pub struct Font {
+    size: u32,
+    width: u32,
+    family: String,
+    italic: bool,
+    bold: bool,
+    font: fontdue::Font,
+}
+
+impl Font {
+    pub fn new(
+        storage: Storage,
+        size: u32,
+        width: u32,
+        family: String,
+        font_scale: Option<f32>,
+        italic: Option<bool>,
+        bold: Option<bool>,
+    ) -> Self {
+        let path_to_font = Path::new(&family);
+        let font_in_bytes = storage.load(&path_to_font).unwrap();
+        let font = fontdue::Font::from_bytes(
+            font_in_bytes,
+            fontdue::FontSettings {
+                scale: font_scale.unwrap_or(fontdue::FontSettings::default().scale),
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        Self {
+            size,
+            width,
+            family,
+            italic: italic.unwrap_or(false),
+            bold: bold.unwrap_or(false),
+            font,
+        }
+    }
 }
