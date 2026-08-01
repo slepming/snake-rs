e3b03088d556d7b1d9c3f1eaabf6e523d91cf784
slepming <siskaura06@gmail.com>
2026-08-01 22:58:34 +0300

    fix defulat value for color. Cuz alpha 1 is invisible

--- src/drw/drawable.rs
+++ src/drw/drawable.rs
@@ -111,7 +111,7 @@ impl Default for DrawableCreateInfo {
                 r: 255,
                 g: 255,
                 b: 255,
-                a: 1,
+                a: 255,
             },
         }
     }
