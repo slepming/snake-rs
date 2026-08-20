2f93851c27a9b16828437dffe47d9ea5aa58c261
slepming <siskaura06@gmail.com>
2026-08-20 23:18:58 +0300

    comment ClassInfo functions

--- src/ecs/tables.rs
+++ src/ecs/tables.rs
@@ -137,6 +137,7 @@ pub struct ClassInfo {
 }
 
 impl ClassInfo {
+    /// Returns [`ClassInfo`] with structure name as class name.
     pub fn of<T: 'static>() -> Self {
         Self {
             type_id: TypeId::of::<T>(),
@@ -144,6 +145,80 @@ impl ClassInfo {
         }
     }
 
+    /// Returns [`ClassInfo`] with a given class name.
+    ///
+    /// It may create unique objects with unique descriptors.
+    /// # Example
+    /// 
+    /// ```rust
+    /// #[game_object]
+    /// struct SecondDrawable {}
+    /// 
+    /// impl GameObject for SecondDrawable {
+    ///     fn update(&mut self, _world: &mut EntityComponent) {}
+    /// 
+    ///     fn start(&mut self, _world: &mut EntityComponent) {}
+    /// }
+    /// 
+    /// #[game_object]
+    /// struct CustomDrawable {
+    /// }
+    /// 
+    /// impl GameObject for CustomDrawable {
+    ///     fn update(&mut self, _world: &mut EntityComponent) {
+    ///     }
+    /// 
+    ///     fn start(&mut self, world: &mut EntityComponent) {
+    ///         // Create an object with unique data
+    ///         let drw = SecondDrawable {
+    ///             color: Rgba8 {
+    ///                 r: 255,
+    ///                 g: 255,
+    ///                 b: 255,
+    ///                 a: 255,
+    ///             },
+    ///             shape: Shapes::Square(ShapeCreateInfo::default().with_radius(f32::MAX)),
+    ///         };
+    ///         let transform = Transform([
+    ///             [50.0, 0.0, 0.0, 0.0],
+    ///             [0.0, 50.0, 0.0, 0.0],
+    ///             [0.0, 0.0, 1.0, 0.0],
+    ///             [600.0, 600.0, 0.0, 1.0],
+    ///         ]);
+    ///
+    ///         // Create a second object with unique data
+    ///         let drw2 = SecondDrawable {
+    ///             color: Rgba8 {
+    ///                 r: 255,
+    ///                 g: 0,
+    ///                 b: 0,
+    ///                 a: 255,
+    ///             },
+    ///             shape: Shapes::Square(ShapeCreateInfo::default().with_radius(0.3)),
+    ///         };
+    ///         let transform2 = Transform([
+    ///             [50.0, 0.0, 0.0, 0.0],
+    ///             [0.0, 50.0, 0.0, 0.0],
+    ///             [0.0, 0.0, 1.0, 0.0],
+    ///             [900.0, 900.0, 0.0, 1.0],
+    ///         ]);
+    /// 
+    ///         // Since the classes drw and drw2 are the same. Data between drw2 and drw are the
+    ///         // same and last object, which will be added(with same classes), will create data for
+    ///         // all objects of this class
+    ///         // world.add(drw2, transform2);
+    ///         // world.add(drw, transform);
+    ///         // To avoid this we can use [`ClassInfo::of_class`] function
+    ///
+    ///         let shape = drw.shape();
+    ///         let shape2 = drw2.shape();
+    ///         let boxed_drw = Box::new(drw);
+    ///         let boxed_drw2 = Box::new(drw2);
+    ///         world.add_with_class(boxed_drw2, transform2, shape2, ClassInfo::of_class::<SecondDrawable>("drw2")); // <-- Here we create a unique class
+    ///         world.add_with_class(boxed_drw, transform, shape, ClassInfo::of_class::<SecondDrawable>("drw")); // <-- Here we create a unique class 
+    ///     }
+    /// }
+    /// ```
     pub fn of_class<T: 'static>(class: &'static str) -> Self {
         Self {
             type_id: TypeId::of::<T>(),
